de39ab6331b5e38852bc53ba6ded684b95dee4e3
VariousMilkshakes <bn.gladstone@gmail.com>
2015-03-24 21:59:53 +0000

    word press broken setup
    doesn't work

---
+++ home.php
@@ -0,0 +1,109 @@
+<?php get_header(); ?>
+<div id="content">
+<div class="titleArea">
+	<div id="titleBox">
+		<h1>THE WORKSHOP</h1>
+	</div>
+</div>
+<div class="projectArea">
+	<div id="projectBox">
+		<h2>OUR PROJECTS</h2>
+		<h4><i>Over ambition and inexperience</i></h4>
+		<div class="projectHolder">
+			<a href="http://www.hadesbroadband.co.uk" class="project smallThumb">
+				<div class="circle">
+					<img src="./images/hadesbroadband_Logo.png" alt="Hades Broadband">
+				</div>
+			</a>
+			<a href="http://sams.hadesoft.io" class="project largeThumb">
+				<div class="circle">
+					<img src="./images/sams_Logo.png" alt="SamS">
+				</div>
+			</a>
+			<a href="https://github.com/Snappey/HadesBot" class="project smallThumb">
+				<div class="circle">
+					<img src="./images/hadesbot_Logo.png" alt="Hades Bot">
+				</div>
+			</a>
+			<a href="https://www.hadesoft.io" class="project largeThumb">
+				<div class="circle">
+					<img src="./images/hadesoft_Logo.png" alt="HadeSoft">
+				</div>
+			</a>
+		</div>
+		<div class="projectHolder">
+			<div class="projectDesc">
+				<section>
+					<a href="http://www.hadesbroadband.co.uk">
+						<h5 class="orangeUnderline">
+							Hades Broadband
+						</h5>
+					</a>
+				</section>
+				<p>
+					Hades Broadband hub for news and Team Speak access.
+				</p>
+			</div>
+			<div class="projectDesc">
+				<section>
+					<a href="http://sams.hadesoft.io">
+						<h5 class="greenUnderline">
+							SamS
+						</h5>
+					</a>
+				</section>
+				<p>
+					Computing Project
+				</p>
+			</div>
+			<div class="projectDesc">
+				<section>
+					<a href="https://github.com/Snappey/HadesBot">
+						<h5 class="redUnderline">
+							Hades Bot
+						</h5>
+					</a>
+				</section>
+				<p>
+					Steam bot
+				</p>
+			</div>
+			<div class="projectDesc">
+				<section>
+					<a href="http://www.hadesoft.io">
+						<h5 class="blueUnderline">
+							HadeSoft
+						</h5>
+					</a>
+				</section>
+				<p>
+					Hades Broadband software hub.
+				</p>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="teamArea">
+	<header>
+		<div class="wrapTop"></div>
+		<div class="wrapSide left"></div>
+		<img src="./images/ourTeam.png" />
+		<div class="wrapSide right"></div>
+	</header>
+	<section class="teamBox">
+		<div class="members">
+			<div class="member">
+				<img src="./images/mem1.png" alt="">
+			</div>
+			<div class="member">
+				<img src="./images/mem2.png" alt="">
+			</div>
+			<div class="member">
+				<img src="./images/mem3.png" alt="">
+			</div>
+		</div>
+	</section>
+</div>
+<div class="futureArea">
+	<h6>Coming Soon</h6>
+<?php get_footer(); ?>
